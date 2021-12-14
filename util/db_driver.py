a7e759f2e20dd0ad98af35cbbbe44d382ad98c03
cyberparty <jc@cyberparty.me>
2021-12-14 21:26:57 +0000

    add database driver

---
+++ util/db_driver.py
@@ -0,0 +1,25 @@
+import aiosqlite
+
+class Database():
+    def __init__(self):
+        self.database = None
+
+    async def __aenter__(self):
+        self.database = await self.database_connection()
+        self.cursor = await self.database.cursor()
+
+    async def __aexit__(self):
+        self.database.close()
+        self.cursor.close()
+
+    async def __call__(self, sql, *args):
+        await self.cursor.execute(sql, *args)
+        await self.database.commit()
+        res = await self.cursor.fetchall()
+
+        if res:
+            return res
+        return None
+
+    async def database_connection(self):
+        return await aiosqlite.connect("database.db")

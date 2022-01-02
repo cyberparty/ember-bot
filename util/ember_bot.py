350d8bafad9807b0037e5f5d7a78ad2c150b89b6
cyberparty <jc@cyberparty.me>
2022-01-02 00:29:40 +0000

    add db to bot class

--- util/ember_bot.py
+++ util/ember_bot.py
@@ -1,11 +1,14 @@
 from discord.ext import commands
 from json import load
+from util.db_driver import Database
 
 class EmberBot(commands.AutoShardedBot):
 
     def __init__(self, **kwargs):
-        with open("./config.json") as file:
+        with open("config.json") as file:
             self.config = load(file)
+
+        self.database = Database
 
         super().__init__(command_prefix=self.config["attributes"]["prefix"],
                          description=self.config["attributes"]["description"],
